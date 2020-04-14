22ebe9e2d4531e40d059f42cf35cba3ac0acb8a2
Ida Granholm <granholm.ida@gmail.com>
2020-04-14 21:17:16 +0200

    Implement Pers code into the weather component (showing search result weather)
    To do: Switch between F and C degrees

--- src/App.js
+++ src/App.js
@@ -5,41 +5,82 @@ import Weather from "./components/Weather";
 
 import "./App.css";
 
-const API_KEY = "61dab6fb7df22a0d9bb49a92808e8af1";
+const API_KEY = "abafff9407e6299f362e6d1a0a127946";
 
 class App extends React.Component {
 
   constructor(props){
     super(props);
-    this.state = {
-      city: "",
-      country: undefined,
-      temperature: undefined,
-      wind: undefined,
-      humidity: undefined,
-      description: undefined,
-      error: undefined
-    }
+    this.state = {};
   }
 
-  navWeather = async (event) => {
+  getWeather = async (event, toggleunit) => {
+    let unit = "";
+    if (this.toggleunit) {
+      unit = "metric";
+    } else {
+      unit = "imperial";
+    }
+    
     const city = event;
     const api_call = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${API_KEY}`
     );
-    const cityWeather = await api_call.json();
+    const data = await api_call.json();
 
-    if (cityWeather.cod === 200) {
+    const deg = convertDir(data.wind.deg);
+    const sunRise = convertTime(data.sys.sunrise);
+    const sunSet = convertTime(data.sys.sunset);
+
+    function convertTime(unixTime) {
+      let dt = new Date(unixTime * 1000);
+      let h = dt.getHours();
+      let m = "0" + dt.getMinutes();
+      let t = h + ":" + m.substr(-2);
+      return t;
+    }
+
+    function convertDir(deg) {
+      let compass = [
+        "N",
+        "NNE",
+        "NE",
+        "ENE",
+        "E",
+        "ESE",
+        "SE",
+        "SSE",
+        "S",
+        "SSW",
+        "SW",
+        "WSW",
+        "W",
+        "WNW",
+        "NW",
+        "NNW",
+        "N",
+      ];
+      const index = Math.round((deg % 360) / 22.5);
+      return compass[index];
+    }
+
+    if (data.cod === 200) {
       this.setState({
-        city: cityWeather.name,
-        country: cityWeather.sys.country,
-        temperature: cityWeather.main.temp,
-        wind: cityWeather.wind.speed,
-        humidity: cityWeather.main.humidity,
-        description: cityWeather.weather[0].description,
-        sunrise: cityWeather.sys.sunrise,
-        sunset: cityWeather.sys.sunset,
+        weather: data.weather["0"].description,
+        icon: data.weather["0"].icon,
+        city: data.name,
+        country: data.sys.country,
+        temp: data.main.temp,
+        feelsLike: data.main.feels_like,
+        humidity: data.main.humidity,
+        wind: data.wind.speed,
+        deg: deg,
+        sunRise: sunRise,
+        sunSet: sunSet,
+        latitude: data.coord.lat,
+        longitude: data.coord.lon,
         error: "",
+        toggleunit: true
       });
     } 
     else {
@@ -49,24 +90,39 @@ class App extends React.Component {
     }
   }
 
+  ToggleUnits = () => {
+    this.setState((state) => ({
+      toggleunit: !state.this.toggleunit,
+      latitude: this.latitude,
+      longitude: this.longitude,
+    }));
+    this.getWeather(this.latitude, this.longitude, this.state.toggleunit);
+  };
+
   render() {
     return (
       <div>
         <Nav 
-          callback={this.navWeather.bind(this)}
+          callback={this.getWeather.bind(this)}
           city={this.state.city}
         />
         <Jumbotron />
         <Weather 
+          weather={this.state.weather}
+          icon={this.state.icon}
           city={this.state.city}
           country={this.state.country}
-          temperature={this.state.temperature}
-          wind={this.state.wind}
+          temp={this.state.temp}
+          feelsLike={this.state.feelsLike}
           humidity={this.state.humidity}
-          description={this.state.description}
-          sunrise={this.state.sunrise}
-          sunset={this.state.sunrise}
+          wind={this.state.wind}
+          deg={this.state.deg}
+          sunRise={this.state.sunRise}
+          sunSet={this.state.sunSet}
+          latitude={this.state.latitude}
+          longitude={this.state.longitude}
           error={this.state.error}
+          toggleunit={this.state.toggleunit}
         />
       </div>
     );
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,11 +1,27 @@
 import React from "react";
 
 class Weather extends React.Component {
+  // constructor(props){
+  //   super(props);
+  //   this.state = {
+  //     toggleunit: this.state.toggleunit
+  //   };
+  // }
+
+  // ToggleUnits = () => {
+  //   if (this.state.toggleunit) {
+  //     this.setState({toggleunit: false})
+  //   } else {
+  //     this.setState({toggleunit: true})
+  //   }
+  // }
+
   render() {
+    console.log(this.state);
     return (
       <div className="container">
         <div className="row">
-          <div className="col-12">
+          <div className="col-⁄⁄⁄⁄⁄⁄⁄⁄">
             <ul className="nav nav-tabs" id="myTab" role="tablist">
               <li className="nav-item">
                 <a
@@ -37,27 +53,71 @@ class Weather extends React.Component {
 
             <div className="tab-content" id="myTabContent">
               <div
-                className="tab-pane fade show active mt-5"
+                className="tab-pane fade show active mt-4"
                 id="current-weathere"
                 role="tabpanel"
                 aria-labelledby="current-weather-tab"
               >
                 {this.props.city && this.props.country && (
-                  <p>
-                    Location: {this.props.city}, {this.props.country}
-                  </p>
+                  <div>
+                    <span className="muted-text">Location: </span>
+                    <h3>
+                      {this.props.city}, {this.props.country}
+                    </h3>
+                  </div>
                 )}
-                {this.props.temperature && (
-                  <p>Temperature: {this.props.temperature}</p>
-                )}
-                {this.props.wind && <p>Wind force: {this.props.wind}</p>}
-                {this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
-                {this.props.description && (
-                  <p>Conditions: {this.props.description}</p>
-                )}
-                {this.props.sunrise && <p>Sunrise: {this.props.sunrise}</p>}
-                {this.props.sunset && <p>Sunset: {this.props.sunset}</p>}
-                {this.props.error && <p>{this.props.error}</p>}
+
+                {this.props.icon &&
+                  this.props.temp &&
+                  this.props.feelsLike &&
+                  this.props.weather && (
+                    <div>
+                      <h4>
+                        {this.props.temp}&deg;{" "}
+                        <span className="text-muted">
+                          , feels like: {this.props.feelsLike}&deg;
+                        </span>
+                      </h4>
+                      <img
+                        src={`http://openweathermap.org/img/wn/${this.props.icon}@2x.png`}
+                        alt="Icon"
+                        width="50"
+                        height="50"
+                      />
+                      <p className="text-capitalize">{this.props.weather}</p>
+                    </div>
+                  )}
+
+                {/* {this.props.temp && this.props.latitude && this.props.longitude &&
+                <button onClick={() => this.ToggleUnits(this.props.toggleunit)}>
+                  {this.props.toggleunit ? "F" : "C"}
+                </button>} */}
+
+                <ul className="list-group">
+                  {this.props.wind && this.props.deg && (
+                    <li className="list-group-item">
+                      Wind force: {this.props.wind} {this.props.deg}
+                    </li>
+                  )}
+                  {this.props.humidity && (
+                    <li className="list-group-item">
+                      Humidity: {this.props.humidity}
+                    </li>
+                  )}
+                  {this.props.sunRise && (
+                    <li className="list-group-item">
+                      Sunrise: {this.props.sunRise}
+                    </li>
+                  )}
+                  {this.props.sunSet && (
+                    <li className="list-group-item">
+                      Sunset: {this.props.sunSet}
+                    </li>
+                  )}
+                  {this.props.error && (
+                    <li className="list-group-item">{this.props.error}</li>
+                  )}
+                </ul>
               </div>
 
               <div
