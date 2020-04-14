dd98da52bc36e5181747376621b684c322af8223
Ida Granholm <granholm.ida@gmail.com>
2020-04-14 22:40:32 +0200

    Rewrite App functions to arrow funtions

--- src/App.js
+++ src/App.js
@@ -5,34 +5,15 @@ import Weather from "./components/Weather";
 
 import "./App.css";
 
-const API_KEY = "abafff9407e6299f362e6d1a0a127946";
-
 class App extends React.Component {
 
   constructor(props){
     super(props);
     this.state = {};
   }
-
-  getWeather = async (event, toggleunit) => {
-    let unit = "";
-    if (this.toggleunit) {
-      unit = "metric";
-    } else {
-      unit = "imperial";
-    }
-    
-    const city = event;
-    const api_call = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${API_KEY}`
-    );
-    const data = await api_call.json();
-
-    const deg = convertDir(data.wind.deg);
-    const sunRise = convertTime(data.sys.sunrise);
-    const sunSet = convertTime(data.sys.sunset);
-
-    function convertTime(unixTime) {
+  
+  // Tool box
+  convertTime = (unixTime) => {
       let dt = new Date(unixTime * 1000);
       let h = dt.getHours();
       let m = "0" + dt.getMinutes();
@@ -40,31 +21,53 @@ class App extends React.Component {
       return t;
     }
 
-    function convertDir(deg) {
-      let compass = [
-        "N",
-        "NNE",
-        "NE",
-        "ENE",
-        "E",
-        "ESE",
-        "SE",
-        "SSE",
-        "S",
-        "SSW",
-        "SW",
-        "WSW",
-        "W",
-        "WNW",
-        "NW",
-        "NNW",
-        "N",
-      ];
-      const index = Math.round((deg % 360) / 22.5);
-      return compass[index];
-    }
+  convertDir = (deg) => {
+    let compass = [
+      "N","NNE","NE",
+      "ENE","E","ESE",
+      "SE","SSE","S",
+      "SSW","SW","WSW",
+      "W","WNW","NW",
+      "NNW","N",
+    ];
+    const index = Math.round((deg % 360) / 22.5);
+    return compass[index];
+  }
 
+  toggleUnits = () => {
+    this.setState((state) => ({
+      toggleunit: !state.this.toggleunit,
+      latitude: this.latitude,
+      longitude: this.longitude,
+    }));
+    this.getWeather(this.latitude, this.longitude, this.state.toggleunit);
+  };
+
+  // API call
+  getWeather = async (query, apiEndpoint = 'weather', toggleunit) => {
+    const apiUrl = 'https://api.openweathermap.org/data/2.5/';
+    const apiKey = 'abafff9407e6299f362e6d1a0a127946';
+    
+    let unit = '';
+    
+    if (toggleunit) {
+      unit = "metric";
+    } else {
+      unit = "imperial";
+    }
+    
+    let api_call = await fetch(
+      apiUrl + apiEndpoint + `?q=${query}&units=${unit}&appid=${apiKey}`
+      //{apiUrl} + {apiEndpoint} + '?q=' + {city} + '&units=' +  {unit} + '&appid=' + {apiKey}
+    );
+    let data = await api_call.json();
+    console.log(data);
+    
     if (data.cod === 200) {
+      const deg = this.convertDir(data.wind.deg);
+      const sunRise = this.convertTime(data.sys.sunrise);
+      const sunSet = this.convertTime(data.sys.sunset);
+    
       this.setState({
         weather: data.weather["0"].description,
         icon: data.weather["0"].icon,
@@ -80,24 +83,14 @@ class App extends React.Component {
         latitude: data.coord.lat,
         longitude: data.coord.lon,
         error: "",
-        toggleunit: true
       });
-    } 
-    else {
+
+    } else {
       this.setState({
         error: "Enter a city"
       });
     }
   }
-
-  ToggleUnits = () => {
-    this.setState((state) => ({
-      toggleunit: !state.this.toggleunit,
-      latitude: this.latitude,
-      longitude: this.longitude,
-    }));
-    this.getWeather(this.latitude, this.longitude, this.state.toggleunit);
-  };
 
   render() {
     return (
